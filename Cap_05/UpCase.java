63eb52f6f4773d8978f2b1dfe777c70efaf5f0ba
Gabriel Bispo <bispogabriel333@gmail.com>
2022-09-21 14:33:56 -0300

    initial commit

---
+++ Cap_05/UpCase.java
@@ -0,0 +1,19 @@
+/*
+Transformar letras minúsculas em maiúscula 
+pela redefinicao do 6o bit com 0.
+*/
+
+class UpCase {
+	public static void main(String[] args) {
+
+		char ch;
+		for(int i = 0; i < 26; i++) {
+			ch = (char) ('a' + i);
+			System.out.print(ch);
+
+			ch = (char) (ch & 65503);
+			System.out.print(ch + " ");
+		}
+
+	}
+}
