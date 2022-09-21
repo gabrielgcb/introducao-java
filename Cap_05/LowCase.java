e0eacfc2e1a76f9bca511083a934ba4028325a8c
Gabriel Bispo <bispogabriel333@gmail.com>
2022-09-21 16:25:04 -0300

    initial commit

---
+++ Cap_05/LowCase.java
@@ -0,0 +1,20 @@
+/*
+Transformar letras maiúsculas em minúsculas 
+pela redefinicao do 6o bit com 1.
+*/
+
+class LowCase{
+	public static void main(String[] args) {
+
+		char ch;
+
+		for(int i = 0; i < 26; i++) {
+			ch = (char) ('A' + i);
+			System.out.print(ch);
+
+			ch = (char) (ch | 32);
+			System.out.print(ch + " ");
+		}
+		
+	}
+}
